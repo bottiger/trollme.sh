4f9365ae627978adceae3a6e34ada204d89078bf
Arvid Böttiger <bottiger@gmail.com>
2024-07-06 00:51:08 +0200

    Create 00_new_aliases.sh

---
+++ shellscript/actions/00_new_aliases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Create a bunch of bogus aliases
+# Credit: https://github.com/Eckankar/prank-script/blob/master/prank.sh
+
+alias ls=`which ln`
+alias ln=`which man`
+alias man=`which cat`
+alias cat=`which htop`
+alias python=`which perl`
+
+alias emacs=`which vim`
+alias emacsclient=`which vim`
+alias vim=`which emacs`
+alias gvim=`which emacs`
+alias vi=`which emacs`
+alias nano=`which gedit`
+alias ed=`which gedit`
+alias gedit=`which nano`
