a0d4bc115df0bd009ac821d6146683d95fcb6bab
Arvid Böttiger <bottiger@gmail.com>
2024-08-01 21:28:25 +0200

    Create 00_bright_terminal_colors.sh

---
+++ shellscript/actions/00_bright_terminal_colors.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Set the terminal colors to some strong almost unreadable colors
+# Found here: https://askubuntu.com/a/1141814
+
+echo -ne '\e]11;#DD00DD\e\\' #hotpink
+echo -ne '\e]10;#00FF00\e\\' # bright green
